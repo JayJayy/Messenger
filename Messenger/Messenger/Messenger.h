008c85f0f98687b120b3efebfd7fcc686429aa08
Johannes Starke <johannesstarke@gmail.com>
2017-01-31 20:56:56 +0100

    Project init

---
+++ Messenger/Messenger/Messenger.h
@@ -0,0 +1,19 @@
+//
+//  Messenger.h
+//  Messenger
+//
+//  Created by Johannes Starke on 31.01.17.
+//  Copyright © 2017 Johannes Starke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Messenger.
+FOUNDATION_EXPORT double MessengerVersionNumber;
+
+//! Project version string for Messenger.
+FOUNDATION_EXPORT const unsigned char MessengerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Messenger/PublicHeader.h>
+
+
